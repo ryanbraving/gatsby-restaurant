ca62ae4c8d59a0305c7997419428b5a113f76ce2
Ryan Braving <ryan.braving@gmail.com>
2019-01-25 16:44:10 -0700

    20190125-NavbarIcons

--- src/components/globals/navbar/NavbarIcons.js
+++ src/components/globals/navbar/NavbarIcons.js
@@ -1,11 +1,79 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
+import {styles} from '../../../utils'
+import {FaFacebook, FaInstagram, FaTelegram, FaYoutube} from 'react-icons/fa'
 
 export default class NavbarIcons extends Component {
+  state = {
+    icons: [
+      { id: 0,
+        icon:<FaFacebook className='icon facebook-icon'/>,
+        path: 'https://www.facebook.com/arianabraving',
+      },
+      { id: 1,
+        icon:<FaInstagram className='icon instagram-icon'/>,
+        path: 'https://www.instagram.com/arianabraving',
+      },
+      { id: 2,
+        icon:<FaTelegram className='icon telegram-icon'/>,
+        path: 'https://t.me/ArianaBraving',
+      },
+      { id: 3,
+        icon:<FaYoutube className='icon youtube-icon'/>,
+        path: 'https://www.youtube.com/results?search_query=arianabraving',
+      },
+    ]
+  }
   render() {
     return (
-      <div>
-        content from navbar icons
-      </div>
+      <IconWrapper>
+        {
+          this.state.icons.map(item => {
+            return(
+              <a 
+                key= {item.id}
+                href= {item.path}
+                target= "_blank"
+                rel= "noopener noreferrer"
+              >
+                {item.icon}
+              </a>
+            )
+          })
+        }
+      </IconWrapper>
     )
   }
 }
+
+const IconWrapper = styled.div`
+.icon{
+  font-size: 1.5rem;
+  coursor:pointer;
+  ${styles.transObject({})};
+  &:hover{
+    color:${styles.colors.mainYellow};
+  }
+}
+
+.facebook-icon{
+  color: #3b579d;
+}
+.instagram-icon{
+  color: #da5f53;
+}
+.telegram-icon{
+  color: #0088cc;
+}
+.youtube-icon{
+  color: #ff0000;
+  // font-size: 1.6rem;
+}
+
+display: none;
+@media (min-width:768px){
+  display: flex;
+  width:10rem;
+  justify-content: space-around;
+}
+`
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,3 @@
 import * as styles from './styles'
-console.log(styles)
 
 export { styles }
