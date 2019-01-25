462e2414449fb4be53527ce14908653f59c16881
Ryan Braving <ryan.braving@gmail.com>
2019-01-25 11:25:23 -0700

    20190125-NavbarLinks CSS

--- src/components/globals/navbar/NavbarLinks.js
+++ src/components/globals/navbar/NavbarLinks.js
@@ -9,22 +9,22 @@ export default class NavbarLinks extends Component {
       {
           id: 0,
           path: '/',
-          name: 'Home',
+          name: 'home',
       },
       {
         id: 1,
         path: '/about',
-        name: 'About',
+        name: 'about',
       },
       {
         id: 2,
         path: '/menu',
-        name: 'Menu',
+        name: 'menu',
       },
       {
         id: 3,
         path: '/contact',
-        name: 'Contact',
+        name: 'contact',
       },
     ]
   }
@@ -48,5 +48,25 @@ export default class NavbarLinks extends Component {
 }
 
 const LinkWrapper = styled.ul`
-
+  li{
+    list-style-type: none;
+  }
+  .nav-link{
+    display: block;
+    text-decoration: none;
+    padding: 0.5rem 1rem 0.5rem 1rem;
+    color: ${styles.colors.mainGrey};
+    font-weight: 700;
+    text-transform: capitalize;
+    cursor: pointer;
+    ${styles.transDefault};  
+    &:hover{
+      background: ${styles.colors.mainGrey};
+      color: ${styles.colors.mainYellow};
+      padding: 0.5rem 1rem 0.5rem 1.3rem;
+    }
+  }
+  height: ${props => (props.open? '152px' : '0px')};
+  overflow: hidden;
+  ${styles.transDefault};
 `
