85fe1fea912ae3a8493269101ac78690cf135e24
Ryan Braving <ryan.braving@gmail.com>
2019-01-24 20:08:54 -0700

    20190124-NavbarHeader

--- src/components/globals/navbar/Navbar.js
+++ src/components/globals/navbar/Navbar.js
@@ -2,15 +2,35 @@ import React, { Component } from 'react'
 import NavbarHeader from './NavbarHeader'
 import NavbarLinks from './NavbarLinks'
 import NavbarIcons from './NavbarIcons'
+import styled from 'styled-components'
 
 export default class Navbar extends Component {
+  state={
+    navbarOpen: false
+  }
+
+  handleNavbar = () =>{
+    this.setState(() =>{
+      console.log("handleNavbar method is called in Navbar")
+      console.log("handleNavbar method is changing the state of navbarOpen to: ", !this.state.navbarOpen)
+      return {navbarOpen:!this.state.navbarOpen}
+    })
+  }
+
   render() {
     return (
-      <div>
-        <NavbarHeader />
-        <NavbarLinks />
+      <NavbWrapper>
+        <NavbarHeader handleNavbar={this.handleNavbar}/>
+        <NavbarLinks navbarOpen={this.state.navbarOpen}/>
         <NavbarIcons />
-      </div>
+      </NavbWrapper>
     )
   }
 }
+
+const NavbWrapper = styled.nav`
+@media (min-width:768px){
+  display:flex;
+  align-items:cent;
+}
+`
--- src/components/globals/navbar/NavbarHeader.js
+++ src/components/globals/navbar/NavbarHeader.js
@@ -1,11 +1,46 @@
 import React, { Component } from 'react'
+import {Link} from 'gatsby'
+import logo from '../../../images/logo.svg'
+
+import { FaAlignRight } from "react-icons/fa"
+import styled from 'styled-components'
 
 export default class NavbarHeader extends Component {
   render() {
+    console.log(logo)
+    const logoPath = logo.replace('http://:8000/','')
+    console.log(logoPath) 
+    const { handleNavbar } = this.props
     return (
-      <div>
-        content from navbar header
-      </div>
+      <HeaderWrapper>
+        <Link to="/">
+          <img src={logoPath} alt="company name" />
+        </Link>
+        <FaAlignRight
+          className="toggle-icon"
+          onClick={() => {
+            console.log("handleNavbar is callbacked inside onClick() function in NavbarHeader")
+            // this.props.handleNavbar()
+            handleNavbar()
+          }}   />
+      </HeaderWrapper>
     )
   }
 }
+
+const HeaderWrapper = styled.div`
+  padding: 0.4rem 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  .toggle-icon{
+    font-size: 1.75rem;
+    color: yellow;
+    cursor: pointer;
+  }
+  @media (min-width: 768px){
+    .toggle-icon{
+      display: none;
+    }
+  }
+`
