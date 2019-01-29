5dcdf3bda20850e1491711fccb774a39d90018b9
Ryan Braving <ryan.braving@gmail.com>
2019-01-28 18:32:05 -0700

    20190128-QuickInfoWrapper-StyledComponent

--- src/components/homePageComponents/QuickInfo.js
+++ src/components/homePageComponents/QuickInfo.js
@@ -1,15 +1,45 @@
 import React, { Component } from 'react'
 import {Section, Title, SectionButton} from '../../utils'
+import styled from 'styled-components'
+import {styles} from '../../utils'
+import {Link} from 'gatsby'
+
 
 export default class QuickInfo extends Component {
   render() {
     return (
       <Section>
           <Title title='our mission' message='let us tell you'></Title>
-          <SectionButton style={{margin: '2rem auto'}}>
-            about
-          </SectionButton>
+          <QuickInfoWrapper>
+            <p className='text'>
+              Amet eiusmod est ut Lorem incididunt ex aute nulla qui duis ullamco 
+              tempor. Laborum adipisicing minim nulla duis commodo ut pariatur minim 
+              consectetur exercitation ex laboris excepteur.
+            </p>
+            <Link to='/about' style={{textDecoration: 'none'}}>
+                <SectionButton style={{margin: '2rem auto'}}> about </SectionButton>
+            </Link>
+          </QuickInfoWrapper>
       </Section>
     )
   }
 }
+
+const QuickInfoWrapper = styled.div`
+  width: 90%;
+  margin: 2rem auto;
+  .text{
+    line-height: 2rem;
+    color: ${styles.colors.mainGrey};
+    word-spacing: 0.2rem;
+  }
+  @media (min-width:768px){
+    width: 70%;
+  }
+  @media (min-width:992px){
+    width: 60%;
+  }
+
+
+
+`
--- src/utils/Section.js
+++ src/utils/Section.js
@@ -4,5 +4,4 @@ export const Section = styled.section`
     padding: 2rem 0;
     width: 90vw;
     margin: 0 auto;
-    /* background:red; */
 `
