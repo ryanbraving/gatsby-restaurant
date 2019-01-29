8aebc2db2b8b83e60a1b90c9b24727f547593482
Ryan Braving <ryan.braving@gmail.com>
2019-01-28 17:33:53 -0700

    20190128-SectionButton

--- src/components/homePageComponents/QuickInfo.js
+++ src/components/homePageComponents/QuickInfo.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
-import {Section, Title} from '../../utils'
+import {Section, Title, SectionButton} from '../../utils'
 
 export default class QuickInfo extends Component {
   render() {
     return (
       <Section>
           <Title title='our mission' message='let us tell you'></Title>
+          <SectionButton style={{margin: '2rem auto'}}>
+            about
+          </SectionButton>
       </Section>
     )
   }
--- src/utils/Button.js
+++ src/utils/Button.js
@@ -17,8 +17,13 @@ const BannerButton = styled.button`
         color: ${styles.colors.mainBlack};
         cursor: pointer;
     }
-
-
 `
-
-export {BannerButton}
+const SectionButton = styled(BannerButton)`
+    color:${styles.colors.mainBlack};
+    ${styles.border({color: `${styles.colors.mainBlack}`})};
+    &:hover{
+            background: ${styles.colors.mainBlack};
+            color: ${styles.colors.mainYellow};
+        }
+    `
+export {BannerButton, SectionButton}
